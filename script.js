b770c4429d6a923ea30eff734e1aea0d01efbbf3
OscarMeneses99 <omenesess1800@alumno.ipn.mx>
2023-03-19 22:48:33 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+    
+    const h1 = document.querySelector('h1');
+    const input1 = document.querySelector('#calculo1');
+    const input2 = document.querySelector('#calculo2');
+    const btn = document.querySelector('#btnCalcular');
+    const pResultado = document.querySelector('#result');
+
+    /*
+    btn.addEventListener('click',btnOnClick);
+
+    function btnOnClick()
+    {
+        const resultado = Number(input1.value) + Number(input2.value);
+        pResultado.innerText= 'Resultado: ' + resultado;
+    }
+    */
+
+    
+
